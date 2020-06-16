5ce995e69d8dbe7e618008b5d1b8bdcb4d42ea24
Fabian Rodriguez <rodrez@MacBook-Pro.fios-router.home>
2020-06-15 21:34:39 -0400

    Added error_program and jargon md

---
+++ LP PT1/game_over.py
@@ -0,0 +1,5 @@
+# The print() function displays the text inside the quotes
+# Python is case-sensitive and by convention functions in Python are lowercase.
+# So if we try Print('Game Over) or PRINT('Game Over) that would not work.
+
+print('Game Over')
